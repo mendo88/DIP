d347cdbbbedd1accf27f3b59696288372c9ed88c
mendo <agu.mendoza@gmail.com>
2020-08-12 22:34:20 -0300

    pdi

--- codigo/Main.py
+++ codigo/Main.py
@@ -12,19 +12,19 @@ from Clasificador import *
 
 
 if __name__ == "__main__":
-    #Flag si elimino suelo o no
+    #Flag si elimino suelo o no / flag if delete floor
     delSuelo = True
     #Path de la imagen
     path = "../train/segmentacion/C1P1E2"
     extension = ".jpg"
 
-    #Cargo la imagen en RGB
+    #Cargo la imagen en RGB / Load image in RBG
     original = cv.imread(path+extension)
 
-    #Reescalo al 30%
+    #Reescalo al 30% / rescale to 30%
     img = cv.resize(original, (-1,-1), None, 0.3, 0.3)
 
-    #Genero una copia en escala de grises
+    #Genero una copia en escala de grises / Make a copy to gray scale
     imgGray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
     #Pre procesamiento, eliminacion de parte del fondo
@@ -42,23 +42,28 @@ if __name__ == "__main__":
 ################################################################################################################
 
     #Segundo procesamiento, eliminacion de algunas hojas y fondo
-    #Segun el caso Se enmascara la imagen con la primer mascara o no y esta es la entrada de este proceso
+    #Segun el caso se enmascara la imagen con la primer mascara o no y esta es la entrada de este proceso
     #Segunda mascara
+
+    # Leaf removal and the background
     mascara = enmascaradoEscalaGrises(imgPrim, imgGray)
     enmasc = cv.bitwise_and(imgPrim, imgPrim, mask=mascara)
 
 ################################################################################################################
 
     #Tercer procesamiento, separacion de hojas y generacion de contornos
+    # Leaf separation and contour generation
+
     contours = enmascaradoPiola(enmasc)
     #Dibujo contornos sobre la primer mascara
     mascara = cv.drawContours(mascara, contours, -1, (0,0,0),2)
 
-    #Busco el contorno de mayor area
+    #Busco el contorno de mayor area / Check for the contour with the higher area
     #Saco la máscara de la hoja principal (area) y su centro
+    # Get the main leaf mask and his center
     componenteMaximaArea, centroMax, boundingData = maxConvex(mascara)
 
-    #Enmascaro la imagen original quedandome con la hoja principal
+    #Enmascaro la imagen original quedandome con la hoja principal / Apply the mask
     result = cv.bitwise_and(img, img, mask=componenteMaximaArea)
     
     #TEST PARA SACAR MASCARA OPTIMA
@@ -83,9 +88,10 @@ if __name__ == "__main__":
     cv.imshow("imgFinal", imgFinal)
     cv.waitKey(0)
     cv.destroyAllWindows()
+    cv.imwrite("image.png", imgFinal)
 
-    #####   PRUEBA DESEMPEÑO  ######
-    #PRUEBA CON GT
+    #####   PRUEBA DESEMPEÑO  ###### Performance test with Intersection over Union
+    #PRUEBA CON GT / TEST WITH GROUND TRUTH
     #pathGT = "_GT.png"
     #imgGT = cv.imread(path+pathGT)
     #imgGT = cv.resize(imgGT, (-1,-1), None, 0.3, 0.3)
